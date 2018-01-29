08c8c36b6f31bbad617729ed56b63926448770b0
aleneus <aleneus@gmail.com>
2018-01-29 12:28:00 +0300

    new version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ here = path.abspath(path.dirname(__file__))
 setup(
     name='espmu',
 
-    version='0.3.0',
+    version='0.3.1',
 
     description='C37.118 (PMU) data parser',
     #long_description=long_description,
